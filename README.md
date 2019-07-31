b5a1eb3bf0c9a09ea514552baca0d4235bc8058e
Daijirou YAMADA <yamad@daiji.ro>
2019-07-31 23:43:25 +0900

    fix some code

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
+dl-generated-svg
+====
+
+## Description
+
+Download a SVG element as SVG file.
 
 
+dl-generated-svg はセレクタで指定したsvg要素の内容をブラウザで保存できるように
+ダウンロードリンクを生成します。すべてJavaScriptで処理するためサーバサイドの処理は不要です。
+
+## Demo
+
+## VS. 
+
+## Requirement
+
+## Usage
+
+```javascript
+import DlSvg from "dl-generated-svg"
+
+const dlSvg = new DlSvg(document.querySelector("#svg-elem"));
+dlSvg.AsSvg("filename.svg");
+```
+
+
+## Install
+
+yarn
+```
+$ yarn add dl-generated-svg
+```
+
+npm
+```
+$ npm i dl-generated-svg
+```
+
+## Contribution
+
+1. Fork it ( http://github.com/dorako321/dl-generated-svg/fork )
+2. Create your feature branch (git checkout -b my-new-feature)
+3. Commit your changes (git commit -am 'Add some feature')
+4. Push to the branch (git push origin my-new-feature)
+5. Create new Pull Request
+
+## Licence
+
+[MIT](https://github.com/tcnksm/tool/blob/master/LICENCE)
+
+## Author
+
+[dorako321](https://github.com/dorako321)
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-export default function downloadSvg(svg: Node, ext: string, fileName: string = 'export.svg') {
-    let a = document.createElement("a");
-    const xml = new XMLSerializer().serializeToString(svg);
-    const blob: string = window.URL.createObjectURL(new Blob([xml], {
-        type: "image/svg+xml"
-    }));
+export default class DlSvg {
+    element: Node;
 
-    a.href = blob;
-    a.download = fileName;
-    a.setAttribute("style", "display: none");
-    document.body.appendChild(a);
-
-    a.click();
-    if (a.parentNode != null) {
-        a.parentNode.removeChild(a);
+    constructor(element: Node) {
+        this.element = element;
     }
-    window.URL.revokeObjectURL(blob);
+    AsSvg(fileName: string = 'export.svg') {
+        let a = document.createElement("a");
+        const xml = new XMLSerializer().serializeToString(this.element);
+        const blob: string = window.URL.createObjectURL(new Blob([xml], {
+            type: "image/svg+xml"
+        }));
+
+        a.href = blob;
+        a.download = fileName;
+        a.setAttribute("style", "display: none");
+        document.body.appendChild(a);
+
+        a.click();
+        if (a.parentNode != null) {
+            a.parentNode.removeChild(a);
+        }
+        window.URL.revokeObjectURL(blob);
+    }
 }
